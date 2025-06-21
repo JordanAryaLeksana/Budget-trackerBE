ef77a72946ce83c1bfd5884d8464b01537b709b5
JordanAryaLeksana <jordanaryaleksana41@gmail.com>
2025-06-22 06:22:20 +0700

    update production

--- src/server.js
+++ src/server.js
@@ -1,7 +1,4 @@
 const app = require('./app');
-const config = require('./config/config');
 const serverless = require('serverless-http');
-const PORT = config.server.port || 5001;
-
 module.exports = serverless(app)
 
